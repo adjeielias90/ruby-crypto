35f4149783dface1e73ceccdee4b5d2b2fd03dfa
David Lanner <git.dhlanner2@gmail.com>
2014-01-22 23:25:53 -0500

    add convenience method String#substitute for quick manual substition checking, for simple ciphers; re-organize stuff; steal and modify Enumerable#frequency from facets gem

--- cryptanalysis.rb
+++ cryptanalysis.rb
@@ -1,27 +1,42 @@
-# require 'facets'
 require 'ascii_charts'
 
 class Array
+
+  LETTER_FREQUENCY_LOOKUP = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+  def sanitize
+    reject { |letter| !LETTER_FREQUENCY_LOOKUP.include? letter.upcase }
+  end
+
   def letter_frequencies
-    # freq = self.frequency
-    # freq.map { |k,v| freq[k] * 100.0/length }
+    # Derived from facets Enumerable#frequency
+    # http://rdoc.info/github/rubyworks/facets/master/Enumerable#frequency-instance_method
     p = Hash.new(0); each{ |v| p[v] += 100*(1.0/self.length)}; p
     pr = Hash.new(0); each{ |v| pr[v] = p[v].round(2) }; pr
   end
 end
 
 class String
+
+  attr_accessor :mappings
+
+  def sanitize
+    split(//).sanitize
+  end
+
   def letter_frequencies
     split(//).letter_frequencies
   end
 
-  def letters
-    letter_frequency_lookup = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-    split(//).reject { |letter| !letter_frequency_lookup.include? letter }
+  def substitute new_mapping
+    puts "\n\n"
+    puts "text"
+    @mappings = {} unless mappings
+    @mappings = Hash[mappings.merge(new_mapping).sort]
+    mappings.each do |ciphertext, plaintext|
+      puts "#{ciphertext} -> #{plaintext}"
+    end
+
+    puts gsub!(new_mapping.keys[0], new_mapping.values[0])
   end
 end
-
-frankenstein = File.read("frankenstein.txt")
-letters = frankenstein.upcase.letters
-frankenstein_lf = letters.letter_frequencies
-puts frankenstein_lf
